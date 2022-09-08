83f73edcc612c26f4f3b89402b796d965f62f3e0
giotto-eth <112372698+giotto-eth@users.noreply.github.com>
2022-09-09 00:29:30 +0200

    Update start_ethminer.sh

--- start_ethminer.sh
+++ start_ethminer.sh
@@ -1,4 +1,4 @@
 #
 # Example shell file for starting ethminer
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./ethminer/ethminer -P stratum1+tcp://0xc4a02f2683d45dad7efb5bd29909b9e4d27eddf7.Rig001@eth-eu1.nanopool.org:9999
+./ethminer/ethminer -P stratum1+tcp://0xff180948d613bABA1959B8D8c9e96b94959828F9.Rig001@eth-eu1.nanopool.org:9999
