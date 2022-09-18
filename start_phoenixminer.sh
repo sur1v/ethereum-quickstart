7d8acd25dac9df7c044f13614961284d348fc4f3
giotto-eth <112372698+giotto-eth@users.noreply.github.com>
2022-09-18 22:48:10 +0200

    Update for ETHW
    Update for ETHW

--- start_phoenixminer.sh
+++ start_phoenixminer.sh
@@ -2,4 +2,4 @@
 # Example shell file for starting PhoenixMiner to mine ETH
 #
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./phoenixminer/PhoenixMiner -pool eth-eu1.nanopool.org:9999 -wal 0xff180948d613bABA1959B8D8c9e96b94959828F9/Rigx001 -pass x
+./phoenixminer/PhoenixMiner -pool ethw-eu1.nanopool.org:15555 -wal 0xff180948d613bABA1959B8D8c9e96b94959828F9/MYRIG001 -pass x
